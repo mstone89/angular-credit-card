db4b50ceee0820dbc8bcc058cf106e269bb9daf4
Molly Stone <molly.stone89@gmail.com>
2020-02-22 09:49:32 -0500

    Add additional inputs for validation

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -9,17 +9,36 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
     name: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3)
-        // Validators.maxLength(5)
-        // Validators.pattern(/\s/)
-      ])
+      Validators.required,
+      Validators.minLength(3)
+      // Validators.maxLength(5)
+      // Validators.pattern(/\s/)
+    ]),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+    ]),
+    expiration: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
+    ]),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3)
+    ])
   });
 
   constructor() {
+    console.log(this.cardForm);
   }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log('form submitted');
   }
 
 }
