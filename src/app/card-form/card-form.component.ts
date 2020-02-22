5a2044f3d13a28c6100002d812d35b33fea4e06f
Molly Stone <molly.stone89@gmail.com>
2020-02-22 10:25:36 -0500

    Create DateFormControl class file

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,44 +1,43 @@
+import { DateFormControl } from './../date-form-control';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-card-form',
-  templateUrl: './card-form.component.html',
-  styleUrls: ['./card-form.component.css']
+    selector: 'app-card-form',
+    templateUrl: './card-form.component.html',
+    styleUrls: ['./card-form.component.css']
 })
 export class CardFormComponent implements OnInit {
-  cardForm = new FormGroup({
-    name: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3)
-      // Validators.maxLength(5)
-      // Validators.pattern(/\s/)
-    ]),
-    cardNumber: new FormControl('', [
-      Validators.required,
-      Validators.minLength(16),
-      Validators.maxLength(16),
-    ]),
-    expiration: new FormControl('', [
-      Validators.required,
-      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
-    ]),
-    securityCode: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(3)
-    ])
-  });
+    cardForm = new FormGroup({
+        name: new FormControl('', [
+            Validators.required,
+            Validators.minLength(3)
+        ]),
+        cardNumber: new FormControl('', [
+            Validators.required,
+            Validators.minLength(16),
+            Validators.maxLength(16),
+        ]),
+        expiration: new DateFormControl('', [
+            Validators.required,
+            Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
+        ]),
+        securityCode: new FormControl('', [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(3)
+        ])
+    });
 
-  constructor() {
-    console.log(this.cardForm);
-  }
+    constructor() {
+        console.log(this.cardForm);
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  onSubmit() {
-    console.log('form submitted');
-  }
+    onSubmit() {
+        console.log('form submitted');
+    }
 
 }
